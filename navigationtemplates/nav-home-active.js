e20846df14421c9345dfa28c9cb241ee90e08088
spencerhhubert <spencerhhubert@gmail.com>
2019-06-27 10:34:08 -0700

    removed store tab

--- navigationtemplates/nav-home-active.js
+++ navigationtemplates/nav-home-active.js
@@ -1,5 +1,5 @@
 document.getElementById('navdivtag1').innerHTML = "<nav class=\"navbar\"><ul class=\"navul\">" ;
 
-document.getElementById('navdivtag2').innerHTML = "<li class=\"navlactive\"><a href=\"index.html\" class=\"navaactive\">Home</a></li>   <li class=\"navl\"><a href=\"members.html\" class=\"nava\">Members</a></li>   <li class=\"navl\"><a href=\"gallery.html\" class=\"nava\">Gallery</a></li>     <li class=\"navl\"><a href=\"store.html\" class=\"nava\">Store</a></li>     <li class=\"navl\"><a href=\"blog.html\" class=\"nava\">Blog</a></li>";
+document.getElementById('navdivtag2').innerHTML = "<li class=\"navlactive\"><a href=\"index.html\" class=\"navaactive\">Home</a></li>   <li class=\"navl\"><a href=\"members.html\" class=\"nava\">Members</a></li>   <li class=\"navl\"><a href=\"gallery.html\" class=\"nava\">Gallery</a></li>          <li class=\"navl\"><a href=\"blog.html\" class=\"nava\">Blog</a></li>";
 
 document.getElementById('navdivtag3').innerHTML = "</ul></nav>" ;
