04eaba5e0195dad641c60cc65d2f9699557621c3
spencerhhubert <spencerhhubert@gmail.com>
2019-08-21 11:23:00 -0700

    Update nav-ononstore-active.js

--- navigationtemplates/nav-ononstore-active.js
+++ navigationtemplates/nav-ononstore-active.js
@@ -1,5 +1,5 @@
 document.getElementById('navdivtag1').innerHTML = "<nav class=\"navbar\"><ul class=\"navul\">" ;
 
-document.getElementById('navdivtag2').innerHTML = "<li class=\"navl\"><a href=\"../../index.html\" class=\"nava\">Home</a></li>   <li class=\"navl\"><a href=\"../../members.html\" class=\"nava\">Members</a></li>   <li class=\"navl\"><a href=\"../../gallery.html\" class=\"nava\">Gallery</a></li>     <li class=\"navlactive\"><a href=\"../index.html\" class=\"navaactive\">Instructions</a></li>  <li class=\"navl\"><a href=\"../../application.html\" class=\"nava\">Apply</a></li>    <li class=\"navl\"><a href=\"../blog.html\" class=\"nava\">Blog</a></li>";
+document.getElementById('navdivtag2').innerHTML = "<li class=\"navl\"><a href=\"../../index.html\" class=\"nava\">Home</a></li>   <li class=\"navl\"><a href=\"../../members.html\" class=\"nava\">Members</a></li>   <li class=\"navl\"><a href=\"../../gallery.html\" class=\"nava\">Gallery</a></li>     <li class=\"navlactive\"><a href=\"../index.html\" class=\"navaactive\">Instructions</a></li>   <li class=\"navl\"><a href=\"../../application.html\" class=\"nava\">Apply</a></li>    <li class=\"navl\"><a href=\"../blog.html\" class=\"nava\">Blog</a></li>";
 
 document.getElementById('navdivtag3').innerHTML = "</ul></nav>" ;
