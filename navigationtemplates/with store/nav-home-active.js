b412426e4acd876d9ff72500f0fcca9c7ece55f2
spencerhhubert <spencerhhubert@gmail.com>
2019-10-12 13:32:44 -0700

    store nav removal

---
+++ navigationtemplates/with store/nav-home-active.js
@@ -0,0 +1,5 @@
+document.getElementById('navdivtag1').innerHTML = "<nav class=\"navbar\"><ul class=\"navul\">" ;
+
+document.getElementById('navdivtag2').innerHTML = "<li class=\"navlactive\"><a href=\"index.html\" class=\"navaactive\">Home</a></li>   <li class=\"navl\"><a href=\"members.html\" class=\"nava\">Members</a></li>   <li class=\"navl\"><a href=\"gallery/index.html\" class=\"nava\">Gallery</a></li>   <li class=\"navl\"><a href=\"instructions/index.html\" class=\"nava\">Instructions</a></li>  <li class=\"navl\"><a href=\"application.html\" class=\"nava\">Apply</a></li>    <li class=\"navl\"><a href=\"blog.html\" class=\"nava\">Blog</a></li>";
+
+document.getElementById('navdivtag3').innerHTML = "</ul></nav>" ;
