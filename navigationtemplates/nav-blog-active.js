22443923f827a940279e3f000b0b0715050059b0
spencerhhubert <spencerhhubert@gmail.com>
2020-01-02 13:22:34 -0800

    added store to navigation

--- navigationtemplates/nav-blog-active.js
+++ navigationtemplates/nav-blog-active.js
@@ -1,5 +1,5 @@
 document.getElementById('navdivtag1').innerHTML = "<nav class=\"navbar\"><ul class=\"navul\">" ;
 
-document.getElementById('navdivtag2').innerHTML = "<li class=\"navl\"><a href=\"index.html\" class=\"nava\">Home</a></li>   <li class=\"navl\"><a href=\"members.html\" class=\"nava\">Members</a></li>   <li class=\"navl\"><a href=\"gallery/index.html\" class=\"nava\">Gallery</a></li>  <li class=\"navl\"><a href=\"application.html\" class=\"nava\">Apply</a></li>    <li class=\"navlactive\"><a href=\"blog.html\" class=\"navaactive\">Blog</a></li>";
+document.getElementById('navdivtag2').innerHTML = "<li class=\"navl\"><a href=\"index.html\" class=\"nava\">Home</a></li>   <li class=\"navl\"><a href=\"members.html\" class=\"nava\">Members</a></li>   <li class=\"navl\"><a href=\"gallery/index.html\" class=\"nava\">Gallery</a></li>   <li class=\"navl\"><a href=\"instructions/index.html\" class=\"nava\">Instructions</a></li>  <li class=\"navl\"><a href=\"application.html\" class=\"nava\">Apply</a></li>    <li class=\"navlactive\"><a href=\"blog.html\" class=\"navaactive\">Blog</a></li>";
 
 document.getElementById('navdivtag3').innerHTML = "</ul></nav>" ;
